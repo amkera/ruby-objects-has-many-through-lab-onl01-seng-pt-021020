00e0986cdfad0576be7729067e6dd1befb170e43
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 22:53:44 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,16 +16,14 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
-  
 
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end
 
   def doctors
-    Appointment.all.map {|appointment| appointment.doctor}
+    self.appointments.map {|appointment| appointment.doctor}
   end
 
-  
 
 end 
