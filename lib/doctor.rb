3bd59050b895807f79b20e57464d8f2b80fd4196
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-03-11 22:43:38 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,31 +1,27 @@
+class Doctor
 
-class Doctor 
   attr_accessor :name
-  
   @@all = []
 
   def initialize(name)
-    @name = name 
+    @name = name
     @@all << self
   end
-  
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+
+  def patients
+    self.appointments.map {|appointment| appointment.patient}
+  end
+
   def self.all
     @@all
   end
-  
-  
-  def appointments
-    Appointment.all.select {|appt| appt.doctor == self} #relationship is now circular with this line of code. 
-    #returns an array of appointments for a particular doctor 
-  end
-  
-  def patients
-    self.appointments.map {|appointment| appointment.patient}
-    #returns an array of all the patients from appointments connected to a specific doctor
-  end
-  
+
   def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
-end 
 
+end 
